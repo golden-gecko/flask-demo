121fc321e34bbf5a87f378be2fa538f5140af4e4
Wojciech Holisz <golden.gecko@holisz.pl>
2024-01-11 14:21:34 +0100

    Upgrade

--- receiver/app.py
+++ receiver/app.py
@@ -1,15 +1,4 @@
 import connexion
-import os
 
-
-def create_app(specification_dir: str) -> tuple:
-    connexion_app = connexion.App(__name__, specification_dir=specification_dir)
-
-    app = connexion_app.app
-    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
-
-    return connexion_app, app
-
-
-connexion_app, app = create_app(specification_dir=os.path.abspath(os.path.dirname(__file__)))
-connexion_app.add_api('v1.yaml', validate_responses=True)
+app = connexion.FlaskApp(__name__)
+app.add_api('v1.yaml')
